ea5e31c2799b7575b8c5db2c9a83080eaf7b7bdc
Mikail Bagishov <bagishov.mikail@yandex.ru>
2021-05-08 21:21:19 +0300

    Add artifact
    Generated image contains pps-cli binary and preconfigured build
    environment.

---
+++ ci/publish-images.sh
@@ -0,0 +1,16 @@
+set -euxo pipefail
+
+# GENERATED FILE DO NOT EDIT
+if [ "$GITHUB_REF" = "refs/heads/master" ]
+then
+  TAG="latest"
+elif [ "$GITHUB_REF" = "refs/heads/trying" ]
+then
+  TAG="dev"
+else
+  echo "unknown GITHUB_REF: $GITHUB_REF"
+  exit 1
+fi
+echo $GITHUB_TOKEN | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
+docker tag pps-cli ghcr.io/jjs-dev/pps-cli:$TAG
+docker push ghcr.io/jjs-dev/pps-cli:$TAG
