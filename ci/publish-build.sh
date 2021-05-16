8373fd46cc6b07097ff0964269a107535493f1fe
Mikail Bagishov <bagishov.mikail@yandex.ru>
2021-05-16 20:04:25 +0300

    add version

--- ci/publish-build.sh
+++ ci/publish-build.sh
@@ -1,3 +1,3 @@
 set -euxo pipefail
 export DOCKER_BUILDKIT=1
-docker build -f build-env/Dockerfile -t pps-cli --build-arg "BUILD_DATE=$(date)" --build-arg "GIT_HASH=$(git rev-parse HEAD)" .
+docker build -f build-env/Dockerfile -t pps-cli --build-arg "BUILD_DATE=$(date)" --build-arg "GIT_HASH=$(git rev-parse HEAD)" --build-arg "VERSION=todo" .
